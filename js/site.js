34ca5b5aa70c920acfade087824d7897ca838b4e
Shovan2024 <2022pms5223@mnit.ac.in>
2023-10-14 15:32:45 +0530

    14/10/2023

--- js/site.js
+++ js/site.js
@@ -80,20 +80,18 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+var slideIndex = 0;
+carousel();
 
-function showDivs(n) {
+function carousel() {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  var x = document.getElementsByClassName("processSlide");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";  
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 20000); // Change image every 20 seconds
 }
